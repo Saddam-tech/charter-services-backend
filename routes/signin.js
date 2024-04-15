ff01ed41fdd1382b767d67b8139e6b4eef275505
Saddam-tech <salokhiddinov0727@gmail.com>
2024-04-16 03:03:46 +0900

    latest changes

--- routes/signin.js
+++ routes/signin.js
@@ -8,7 +8,11 @@ const { createJWT } = require("../utils/util");
 /* POST admin-signin */
 router.post("/signin", async function (req, res) {
   let { username, password } = req.body;
-  console.log({ username, password });
+  console.log(messages.ADMIN_LOGIN, { username, password });
+  if (!username || password) {
+    senderr(res, messages.USER_NOT_FOUND);
+    return;
+  }
   const accessToken = await createJWT({ username, password });
   if (accessToken) return sendresp(res, messages.SUCCESS, null, accessToken);
   else senderr(res, messages.USER_NOT_FOUND);
