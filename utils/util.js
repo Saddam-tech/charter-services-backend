390e3654aeceba26dd62588043d5eb62d8e5dfb4
Saddam-tech <salokhiddinov0727@gmail.com>
2024-04-25 02:09:21 +0900

    latest changes

--- utils/util.js
+++ utils/util.js
@@ -36,7 +36,7 @@ async function createJWT({ userinfo, jfilter = {} }) {
 }
 
 function verifyJWT(req, res, next) {
-  const authHeader = req.headers["authorization"];
+  const authHeader = req.headers["Authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.sendStatus(401);
   jwt.verify(token, secretKey, (err, userinfo) => {
